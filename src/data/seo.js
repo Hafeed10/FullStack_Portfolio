7b2260cd3546c7263266c40bee8349a8a1d4b835
Hafeed10 <hafeex@gamil.com>
2024-12-16 17:39:58 +0530

    first and last commit

---
+++ src/data/seo.js
@@ -0,0 +1,37 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a Full-stack web and mobile app developer, with expertise in Python . I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Hafeex", "Hafeex N", "Hafeex Palakkad"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a Full-stack web and mobile app developer, with expertise in Python . I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+			keywords: ["Hafeex", "Hafeex N", "Hafeex Palakkad"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+			keywords: ["Hafeex", "Hafeex N", "Hafeex Palakkad"],	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+			keywords: ["Hafeex", "Hafeex N", "Hafeex Palakkad"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+			keywords: ["Hafeex", "Hafeex N", "Hafeex Palakkad"],
+	},
+];
+
+export default SEO;
